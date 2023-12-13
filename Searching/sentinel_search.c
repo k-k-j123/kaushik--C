acb93ef65a48c58b97042430f99d96fea1f2b8e0
kaushik <joshikaushik419@gmail.com>
2023-12-13 12:05:21 +0530

    minor reformatting

--- Searching/sentinel_search.c
+++ Searching/sentinel_search.c
@@ -1,45 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
-void search(int arr[],int n,int key)
+#include <stdio.h>
+#include <stdlib.h>
+void search(int arr[], int n, int key)
 {
- int last=arr[n-1];
- arr[n-1]=key;
- int i=0;
- while(arr[i]!=key)
- {
-  i++;
- }
- arr[n-1]=last;
- if((i<n-1)||(arr[n-1]==key))
- {
- printf("element is present at %d\n",i);
- }
- else 
- {
- printf("element not found\n");
- }
+    int last = arr[n - 1];
+    arr[n - 1] = key;
+    int i = 0;
+    while (arr[i] != key)
+    {
+        i++;
+    }
+    arr[n - 1] = last;
+    if ((i < n - 1) || (arr[n - 1] == key))
+    {
+        printf("element is present at %d\n", i);
+    }
+    else
+    {
+        printf("element not found\n");
+    }
 }
 int main()
 {
- int arr[30],n,key,i;
- printf("\nenter size of array:\t");
- scanf("%d",&n);
- if(n>30)
- {
-    printf("\nenter no of element less than 30");
-     return -1;
- }
- for(i=0;i<n;i++)
- {
-  arr[i]=rand()%100;
- }
- for(i=0;i<n;i++)
- {
- printf("%d\t",arr[i]);
- }
- printf("\nenter the element to search\t");
- scanf("%d",&key);
+    int arr[30], n, key, i;
+    printf("\nenter size of array:\t");
+    scanf("%d", &n);
+    if (n > 30)
+    {
+        printf("\nenter no of element less than 30");
+        return -1;
+    }
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = rand() % 100;
+    }
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    printf("\nenter the element to search\t");
+    scanf("%d", &key);
 
- search(arr,n,key);
- return 0;
+    search(arr, n, key);
+    return 0;
 }
