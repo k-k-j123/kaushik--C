6f94e603321d53b377d7dc822e1baf09111ef69d
Kaushik <joshikaushik419@gmail.com>
2024-10-09 18:07:34 +0530

    simplified programs

--- OS/FCFS.c
+++ OS/FCFS.c
@@ -1,104 +1,74 @@
-#include<stdio.h>
-#include<string.h>
-struct process{
-	char PName[5];
-	int AT,BT,TAT,WT,CT;
-	int tempBT;
-}P[10],tempStruct;
-int N;
+#include <stdio.h>
+#include <string.h>
 
-void Input(){
-	int i;
-	printf("\nenter number of process:");
-	scanf("%d",&N);
-	for(i=0;i<N;i++){
-		printf("\nenter details of process %d",i+1);
-		printf("\nenter process name:");
-		scanf("%s",P[i].PName);
-		printf("enter AT:");
-		scanf("%d",&P[i].AT);
-		printf("enter BT:");
-		scanf("%d",&P[i].BT);
-		P[i].tempBT=P[i].BT;
-	}
+#define MAX 10
+
+struct Process {
+    char name[5];
+    int at, bt, ct, tat, wt;
+};
+
+struct Process p[MAX];
+int n;
+
+void input() {
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter process %d details:\n", i + 1);
+        scanf("%s %d %d", p[i].name, &p[i].at, &p[i].bt);
+    }
 }
 
-void sortProcessAT(){
-	int i;
-	int j;
-	int tval;
-	char temp[5];
-	for(i=0;i<N;i++){
-		for(j=0;j<N;j++){
-			tempStruct=P[i];
-			P[i]=P[j];
-			P[j]=tempStruct;
-		}
-
-	}
+void sort() {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (p[i].at > p[j].at) {
+                struct Process temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
 }
 
-void output(){
-	int i;
-	int totalTAT=0;
-	int totalWT=0;
-	for(i=0;i<N;i++){
-		P[i].TAT=P[i].CT-P[i].AT;
-		totalTAT=totalTAT+P[i].TAT;
-
-		P[i].WT=P[i].TAT-P[i].BT;
-		totalWT=totalWT+P[i].WT;
-	}
-	printf("\nProcess Details");
-	printf("\n************************************");
-	printf("\nPname\tAT\tBT\tTAT\tWT");
-	printf("\n************************************");
-	for(i=0;i<N;i++){
-		printf("\n%s\t%d\t%d\t%d\t%d",P[i].PName,P[i].AT,P[i].BT,P[i].TAT,P[i].WT);
-	}
-	printf("\n************************************");
-	printf("\nAvg TAT:%d/%d",totalTAT,N);
-	printf("\nAvg WT:%d/%d",totalWT,N);
+void fcfs() {
+    int time = 0;
+    for (int i = 0; i < n; i++) {
+        if (p[i].at > time) {
+            printf("|%d # ", time);
+            time = p[i].at;
+        }
+        printf("|%d %s ", time, p[i].name);
+        time += p[i].bt;
+        printf("%d|", time);
+        p[i].ct = time;
+    }
 }
 
-void FCFS(){
-	int time=0;
-	int finish=0;
-	int i;
-	int flag=0;
-	while(finish!=1){
-		flag=0;
-		for(i=0;i<N;i++){
-			if(P[i].AT<=time && P[i].tempBT!=0)
-			{
-				printf("|%d %s",time,P[i].PName);
-				time=time+P[i].tempBT;
-				printf(" %d|",time);
-				P[i].tempBT=0;
-				P[i].CT=time;
-				flag=1;
-			}
-		}
-		if(flag==0){
-			printf("|%d # ",time);
-			time++;
-			printf(" %d|",time);
-		}
-		for(i=0;i<N;i++){
-			if(P[i].tempBT==0)
-				continue;
-			else
-				break;
-		}
-		if(i==N)
-			finish=1;
-	}
+void output() {
+    int totalTat = 0, totalWt = 0;
+    printf("\nProcess Details\n");
+    printf("Name\tAT\tBT\tCT\tTAT\tWT\n");
+
+    for (int i = 0; i < n; i++) {
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
+        totalTat += p[i].tat;
+        totalWt += p[i].wt;
+        printf("%s\t%d\t%d\t%d\t%d\t%d\n", p[i].name, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt);
+    }
+
+    printf("Average TAT: %.2f\n", (float) totalTat / n);
+    printf("Average WT: %.2f\n", (float) totalWt / n);
 }
 
-int main(){
-	Input();
-	sortProcessAT();
-	FCFS();
-	output();
-	return 0;
+int main() {
+    input();
+    sort();
+    fcfs();
+    output();
+
+    return 0;
 }
--- OS/list.c
+++ OS/list.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<unistd.h>
-#include<dirent.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
 void list(char c,char *dn)
 {
   DIR *dir;
